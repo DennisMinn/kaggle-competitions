e9bd24601868e5584c7d205d0a0c5a04f49277e8
Dennis Minn <minndennis@gmail.com>
2024-06-24 18:24:02 -0400

    added llm features

--- automatic-essay-scoring-2/datamodule/preprocessing.py
+++ automatic-essay-scoring-2/datamodule/preprocessing.py
@@ -3,9 +3,20 @@ import string
 import spacy
 import polars as pl
 import pandas as pd
+import pkg_resources
+from datasets import Dataset
+from transformers import (
+    Trainer,
+    AutoTokenizer,
+    AutoModelForSequenceClassification
+)
 
-with open('data/vocabulary.txt') as infile:
-    vocabulary = set(word.strip().lower() for word in infile)
+vocabulary = {
+    word.strip().lower()
+    for word in pkg_resources.resource_string(__package__, 'vocabulary.txt')
+    .decode('utf-8')
+    .splitlines()
+}
 
 nlp = spacy.load('en_core_web_sm')
 
@@ -55,8 +66,8 @@ def remove_punctuation(text):
 def create_paragraph_features(df):
     df = df.with_columns(
         pl.col('full_text')
-        .str.split(by="\n\n")
-        .alias("paragraph")
+        .str.split(by='\n\n')
+        .alias('paragraph')
     )
 
     # Expand the paragraph list into several lines of data
@@ -71,20 +82,20 @@ def create_paragraph_features(df):
         pl.col('paragraph')
         .map_elements(remove_punctuation)
         .map_elements(count_spelling_errors)
-        .alias("paragraph_error_num")
+        .alias('paragraph_num_errors')
     )
 
     # Calculate the number of sentences and words in each paragraph
     df = df.with_columns(
         pl.col('paragraph')
         .map_elements(lambda paragraph: len(paragraph))
-        .alias("paragraph_len"),
-        pl.col('paragraph')
-        .map_elements(lambda paragraph: len(paragraph.split('.')))
-        .alias("paragraph_sentence_cnt"),
+        .alias('paragraph_num_characters'),
         pl.col('paragraph')
         .map_elements(lambda paragraph: len(paragraph.split(' ')))
-        .alias("paragraph_word_cnt"),
+        .alias('paragraph_num_words'),
+        pl.col('paragraph')
+        .map_elements(lambda paragraph: len(paragraph.split('.')))
+        .alias('paragraph_num_sentences'),
     )
 
     # Aggreate features
@@ -92,35 +103,27 @@ def create_paragraph_features(df):
 
     feature_aggregates += [
         pl.col('paragraph')
-        .filter(pl.col('paragraph_len') >= i)
+        .filter(pl.col('paragraph_num_characters') >= i)
         .count()
-        .alias(f"paragraph_{i}_cnt")
+        .alias(f'paragraph_num_character_greater_equal_{i}')
         for i in [0, 50, 75, 100, 125, 150, 175, 200, 250, 300, 350, 400, 500, 600, 700]
     ]
 
-    feature_aggregates += [
-        pl.col('paragraph')
-        .filter(pl.col('paragraph_len') <= i)
-        .count()
-        .alias(f"paragraph_{i}_cnt")
-        for i in [25,49]
-    ]
-
-    paragraph_feature = ['paragraph_len','paragraph_sentence_cnt','paragraph_word_cnt', 'paragraph_error_num']
+    paragraph_feature = ['paragraph_num_characters','paragraph_num_words','paragraph_num_sentences', 'paragraph_num_errors']
     for feature in paragraph_feature:
         feature_aggregates += [
             pl.col(feature).max().alias(f'{feature}_max'),
-            pl.col(feature).min().alias(f"{feature}_min"),
-            pl.col(feature).mean().alias(f"{feature}_mean"),
-            pl.col(feature).sum().alias(f"{feature}_sum"),
-            pl.col(feature).first().alias(f"{feature}_first"),
-            pl.col(feature).last().alias(f"{feature}_last"),
-            pl.col(feature).kurtosis().alias(f"{feature}_kurtosis"),
-            pl.col(feature).quantile(0.25).alias(f"{feature}_q1"),
-            pl.col(feature).quantile(0.75).alias(f"{feature}_q3")
+            pl.col(feature).min().alias(f'{feature}_min'),
+            pl.col(feature).mean().alias(f'{feature}_mean'),
+            pl.col(feature).sum().alias(f'{feature}_sum'),
+            pl.col(feature).first().alias(f'{feature}_first'),
+            pl.col(feature).last().alias(f'{feature}_last'),
+            pl.col(feature).kurtosis().alias(f'{feature}_kurtosis'),
+            pl.col(feature).quantile(0.25).alias(f'{feature}_q1'),
+            pl.col(feature).quantile(0.75).alias(f'{feature}_q3')
         ]
 
-    df = df.group_by(['essay_id'], maintain_order=True).agg(feature_aggregates).sort("essay_id")
+    df = df.group_by(['essay_id'], maintain_order=True).agg(feature_aggregates).sort('essay_id')
     df = df.to_pandas()
     return df
 
@@ -128,8 +131,8 @@ def create_word_features(df):
     df = df.with_columns(
         pl.col('full_text')
         .map_elements(sanitize_text)
-        .str.split(by=" ").
-        alias("word")
+        .str.split(by=' ').
+        alias('word')
     )
 
     df = df.explode('word')
@@ -137,31 +140,31 @@ def create_word_features(df):
     df = df.with_columns(
         pl.col('word')
         .map_elements(lambda x: len(x))
-        .alias("word_len")
+        .alias('word_length')
     )
 
-    df = df.filter(pl.col('word_len') != 0)
+    df = df.filter(pl.col('word_length') != 0)
 
     # Aggregate features
     feature_aggregates = []
     feature_aggregates += [
         pl.col('word')
-        .filter(pl.col('word_len') >= i + 1)
+        .filter(pl.col('word_length') >= i + 1)
         .count()
-        .alias(f"word_{i + 1}_cnt")
+        .alias(f'word_length_greater_equal_{i + 1}')
         for i in range(15)
     ]
 
     feature_aggregates += [
-        pl.col('word_len').max().alias(f"word_len_max"),
-        pl.col('word_len').mean().alias(f"word_len_mean"),
-        pl.col('word_len').std().alias(f"word_len_std"),
-        pl.col('word_len').quantile(0.25).alias(f"word_len_q1"),
-        pl.col('word_len').quantile(0.50).alias(f"word_len_q2"),
-        pl.col('word_len').quantile(0.75).alias(f"word_len_q3"),
+        pl.col('word_length').max().alias(f'word_length_max'),
+        pl.col('word_length').mean().alias(f'word_length_mean'),
+        pl.col('word_length').std().alias(f'word_length_std'),
+        pl.col('word_length').quantile(0.25).alias(f'word_length_q1'),
+        pl.col('word_length').quantile(0.50).alias(f'word_length_q2'),
+        pl.col('word_length').quantile(0.75).alias(f'word_length_q3'),
     ]
 
-    df = df.group_by(['essay_id'], maintain_order=True).agg(feature_aggregates).sort("essay_id")
+    df = df.group_by(['essay_id'], maintain_order=True).agg(feature_aggregates).sort('essay_id')
     df = df.to_pandas()
     return df
 
@@ -193,3 +196,28 @@ def create_bag_of_words_features(df, vectorizer, stage):
     bow_df.columns = [f'tfidf_{i}' for i in range(bow_df.shape[1])]
     bow_df['essay_id'] = df['essay_id']
     return bow_df
+
+def create_llm_features(df, model_id):
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    model = AutoModelForSequenceClassification.from_pretrained(model_id)
+
+    df = df.to_pandas()
+    dataset = Dataset.from_pandas(df)
+    dataset = dataset.map(
+        tokenizer,
+        fn_kwargs={'truncation': True, 'max_length': 1024},
+        input_columns='full_text'
+    )
+
+    trainer = Trainer(
+        model=model,
+        tokenizer=tokenizer
+    )
+
+    predictions = trainer.predict(dataset).predictions
+    llm_df = pd.DataFrame({
+        'essay_id': df['essay_id'],
+        f'{model_id}_score': predictions
+    })
+
+    return llm_df
