5b075835bdd95c45bce76b3ded7933af21c06e74
Dennis Minn <minndennis@gmail.com>
2024-06-29 16:24:42 -0400

    small metric updates

--- automatic-essay-scoring-2/model/metric.py
+++ automatic-essay-scoring-2/model/metric.py
@@ -1,12 +1,5 @@
 import numpy as np
-from dataclasses import dataclass, field
-from sklearn.metrics import (
-    accuracy_score,
-    recall_score,
-    precision_score,
-    f1_score,
-    cohen_kappa_score
-)
+from sklearn.metrics import cohen_kappa_score
 
 a = 2.998
 b = 1.092
@@ -21,22 +14,26 @@ def quadratic_weighted_kappa_objective(y_true, y_pred):
     labels = y_true + a
     preds = y_pred + a
     preds = preds.clip(1, 6)
-    f = 1/2*np.sum((preds-labels)**2)
-    g = 1/2*np.sum((preds-a)**2+b)
+    f = 1/2 * np.sum((preds - labels)**2)
+    g = 1/2*np.sum((preds - a)**2 + b)
     df = preds - labels
     dg = preds - a
-    grad = (df/g - f*dg/g**2)*len(labels)
+    grad = (df/g - f * dg / g**2) * len(labels)
     hess = np.ones(len(labels))
     return grad, hess
 
-def compute_metrics(output):
-    labels = output.label_ids
-    predictions = output.predictions.clip(0, 5).round(0)
+def quadratic_weighted_kappa_wrapper(task):
+    assert task in ['regression', 'classification']
 
-    return {
-        'accuracy': accuracy_score(labels, predictions, normalize=True),
-        'recall': recall_score(labels, predictions, average='macro'),
-        'precision': precision_score(labels, predictions, average='macro'),
-        'f1': f1_score(labels, predictions, average='macro'),
-        'quadratic_weighted_kappa': cohen_kappa_score(labels, predictions, weights='quadratic')
-    }
+    def quadratic_weighted_kappa_score(eval_pred):
+        predictions, labels = eval_pred
+        if task == 'regression':
+            quadratic_weighted_kappa = cohen_kappa_score(labels, predictions.clip(0,5).round(0), weights='quadratic')
+        else:
+            quadratic_weighted_kappa = cohen_kappa_score(labels, predictions.argmax(-1), weights='quadratic')
+        results = {
+            'quadratic_weighted_kappa': quadratic_weighted_kappa
+        }
+        return results
+
+    return quadratic_weighted_kappa_score
